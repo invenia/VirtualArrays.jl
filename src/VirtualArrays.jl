518dd3ded90916d2535d12be74f4485b07ef1477
Samuel Massinon <samuel.massinon@invenia.ca>
2016-01-07 13:53:32 -0600

    Added info on which dimension the expansions should be on

--- src/VirtualArrays.jl
+++ src/VirtualArrays.jl
@@ -5,13 +5,16 @@ export VirtualArray, getindex, setindex!, size, length
 import Base.size, Base.getindex, Base.length, Base.setindex!
 
 type VirtualArray{T,N} <: AbstractArray{Array{T,N},1}
+    expanded_dim::UInt # This is the dimension we expand along
     parents::Array{Array{T,N},1}
 
+    function VirtualArray(expanded_dim::UInt, parents::AbstractArray{T,N}...)
+        return new(expanded_dim, Array[parent for parent in parents])
+    end
     function VirtualArray(parents::AbstractArray{T,N}...)
-        return new(Array[parent for parent in parents])
+        return new(1, Array[parent for parent in parents])
     end
 end
-
 
 function size(V::VirtualArray)
     total = 0
