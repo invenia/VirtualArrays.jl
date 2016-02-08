715935ddc4ba6575a2063eca576e319e6f7e21bd
Samuel Massinon <samuel.massinon@invenia.ca>
2016-02-08 15:54:59 -0600

    Added tests for range values, setting and getting

--- test/VirtualArrays.jl
+++ test/VirtualArrays.jl
@@ -412,7 +412,7 @@ facts("Creating VirtualArrays oddly") do
     context("storing float array into a real VirtualArray") do
         # set up
         num_dims = 1
-        len = rand(2:10) # no larger than 5
+        len = rand(2:10)
         expanded_dim = 1
 
         a = rand(Float64, len)
@@ -1399,5 +1399,235 @@ facts("Check the memory usage of VirtualArray") do
         a = collect(1:10000000)
         test = @allocated VirtualArray{Int, 1}(1,a)
         @fact test --> expected
+    end
+end
+
+@testset "VirtualArrays Tests" begin
+    @testset "range getindex" begin
+        @testset "getting a range value in one 1 d parent" begin
+            # set up
+            num = rand(1:1000)
+            num_parents = rand(3:10)
+            len = rand(1:100)
+            change_p = rand(1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(change_start:len)
+            change_range = change_start:change_end
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, collect(num:num+len-1))
+            end
+
+            expected = cat(1, parents...)
+            test = VirtualArray{Int, 1}(parents...)
+
+            @test test.parents[change_p][change_range] == test[(change_p-1)*len+change_range]
+            @test test[(change_p-1)*len+change_range] == expected[(change_p-1)*len+change_range]
+            @test test == expected
+        end
+        @testset "getting a range value in multi 1 d parent" begin
+            # set up
+            num = rand(1:1000)
+            num_parents = rand(3:10)
+            len = rand(1:100)
+            change_p_start = rand(1:num_parents-1)
+            change_p_end = rand(change_p_start+1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(1:len)
+            change_range = (change_p_start-1)*len+change_start:(change_p_end-1)*len+change_end
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, collect(num:num+len-1))
+            end
+
+            expected = cat(1, parents...)
+            test = VirtualArray{Int, 1}(parents...)
+
+            @test test[change_range] == expected[change_range]
+            @test test == expected
+        end
+        @testset "getting a range value in multi multi d parent" begin
+
+            # set up
+            # keep these numbers small because we can run out of memory or get very slow tests
+            num_parents = rand(3:10) # no larger than 10
+            num_dims = rand(3:6) # no larger than 6
+            len = rand(2:5) # no larger than 5
+            expanded_dim = rand(1:num_dims)
+
+            dims = zeros(Int, num_dims) + len
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, rand(dims...))
+            end
+            change_to = rand(1:10)
+            change_p_start = rand(1:num_parents-1)
+            change_p_end = rand(change_p_start+1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(1:len)
+            change_range_expanded_dim = (change_p_start-1)*len+change_start:(change_p_end-1)*len+change_end
+
+            change_range = []
+            for i in 1:num_dims
+                if i != expanded_dim
+                    change_start = rand(1:len)
+                    change_end = rand(change_start:len)
+                    push!(change_range, change_start:change_end)
+                else
+                    push!(change_range, change_range_expanded_dim)
+                end
+            end
+
+            expected = cat(expanded_dim, parents...)
+            test = VirtualArray{Float64, num_dims}(expanded_dim, parents...)
+
+            @test test[change_range...] == expected[change_range...]
+            @test test == expected
+
+        end
+        @testset "getting a range value in multi M d parent like an N d range " begin
+
+            # set up
+            # keep these numbers small because we can run out of memory or get very slow tests
+            num_parents = rand(3:10) # no larger than 10
+            num_dims = rand(3:6) # no larger than 6
+            len = rand(2:5) # no larger than 5
+            expanded_dim = rand(1:num_dims)
+
+            dims = zeros(Int, num_dims) + len
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, rand(dims...))
+            end
+            change_to = rand(1:10)
+            change_p_start = rand(1:num_parents-1)
+            change_p_end = rand(change_p_start+1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(1:len)
+            change_range_expanded_dim = (change_p_start-1)*len+change_start:(change_p_end-1)*len+change_end
+
+            range_dim = rand(1:num_dims-1) # there's a chance we don't have an index on the expanded_dim
+
+            change_range = []
+            for i in 1:range_dim-1
+                if i != expanded_dim
+                    change_start = rand(1:len)
+                    change_end = rand(change_start:len)
+                    push!(change_range, change_start:change_end)
+                else
+                    push!(change_range, change_range_expanded_dim)
+                end
+            end
+
+            expected = cat(expanded_dim, parents...)
+            test = VirtualArray{Float64, num_dims}(expanded_dim, parents...)
+
+            end_left = 1
+            for i in range_dim:num_dims
+                end_left *= size(test)[i]
+            end
+
+            push!(change_range, 1:end_left)
+
+            @test test[change_range...] == expected[change_range...]
+            @test test == expected
+
+        end
+    end
+    @testset "range setindex" begin
+        @testset "setting a range value in one 1 d parent" begin
+            # set up
+            num = rand(1:1000)
+            num_parents = rand(3:10)
+            len = rand(1:100)
+            change_p = rand(1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(change_start:len)
+            change_range = change_start:change_end
+            change_to = rand(1:10)
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, collect(num:num+len-1))
+            end
+
+            test = VirtualArray{Int, 1}(parents...)
+            test[change_range] = change_to
+            expected = cat(1, parents...)
+
+            @test test.parents[change_p][change_range] == test[(change_p-1)*len+change_range]
+            @test test[(change_p-1)*len+change_range] == expected[(change_p-1)*len+change_range]
+            @test test == expected
+        end
+        @testset "setting a range value in multi 1 d parent" begin
+            # set up
+            num = rand(1:1000)
+            num_parents = rand(3:10)
+            len = rand(1:100)
+            change_p_start = rand(1:num_parents-1)
+            change_p_end = rand(change_p_start+1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(1:len)
+            change_range = (change_p_start-1)*len+change_start:(change_p_end-1)*len+change_end
+            change_to = rand(1:10)
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, collect(num:num+len-1))
+            end
+
+            test = VirtualArray{Int, 1}(parents...)
+            test[change_range] = change_to
+            expected = cat(1, parents...)
+
+            @test test[change_range] == expected[change_range]
+            @test test == expected
+        end
+        @testset "setting a range value in multi multi d parent" begin
+
+            # set up
+            # keep these numbers small because we can run out of memory or get very slow tests
+            num_parents = rand(3:10) # no larger than 10
+            num_dims = rand(3:6) # no larger than 6
+            len = rand(2:5) # no larger than 5
+            expanded_dim = rand(1:num_dims)
+
+            dims = zeros(Int, num_dims) + len
+
+            parents = []
+            for i in 1:num_parents
+                push!(parents, rand(dims...))
+            end
+            change_to = rand(1:10)
+            change_p_start = rand(1:num_parents-1)
+            change_p_end = rand(change_p_start+1:num_parents)
+            change_start = rand(1:len)
+            change_end = rand(1:len)
+            change_range_expanded_dim = (change_p_start-1)*len+change_start:(change_p_end-1)*len+change_end
+
+            change_range = []
+            for i in 1:num_dims
+                if i != expanded_dim
+                    change_start = rand(1:len)
+                    change_end = rand(change_start:len)
+                    push!(change_range, change_start:change_end)
+                else
+                    push!(change_range, change_range_expanded_dim)
+                end
+            end
+            change_to = rand(1:10)
+
+            test = VirtualArray{Float64, num_dims}(expanded_dim, parents...)
+            test[change_range...] = change_to
+            expected = cat(expanded_dim, parents...)
+
+            @test test[change_range...] == expected[change_range...]
+            @test test == expected
+
+        end
     end
 end
